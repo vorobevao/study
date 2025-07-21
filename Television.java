fd683a38673662686f0554a9bab4f7f9754b3cc3
vorobevao <osvstv@mail.ru>
2025-07-21 15:33:04 +0300

    Update Television.java

--- Television.java
+++ Television.java
@@ -1,15 +1,22 @@
 public class Television {
+    
+    private String brand;
     private String model;
     private String serialNumber;
     private double price;
 
-
-    public Television(String model, String serialNumber, double price) {
+    
+    public Television(String brand, String model, String serialNumber, double price) {
+        this.brand = brand;
         this.model = model;
         this.serialNumber = serialNumber;
         this.price = price;
     }
 
+
+    public String getBrand() {
+        return brand;
+    }
 
     public String getModel() {
         return model;
@@ -21,6 +28,11 @@ public class Television {
 
     public double getPrice() {
         return price;
+    }
+
+
+    public void setBrand(String brand) {
+        this.brand = brand;
     }
 
     public void setModel(String model) {
@@ -35,9 +47,11 @@ public class Television {
         this.price = price;
     }
 
+
     @Override
     public String toString() {
-        return "Телевизор: " + model +
+        return "Бренд: " + brand +
+                "\nМодель: " + model +
                 "\nСерийный номер: " + serialNumber +
                 String.format("\nСтоимость: $%.2f", price);
     }
@@ -45,11 +59,11 @@ public class Television {
 class App {
     public static void main(String[] args) {
 
-        Television tv1 = new Television("Samsung 123", "SN-SAM-12345", 1299.99);
-        Television tv2 = new Television("LG TOP", "SN-LG-67890", 1499.00);
-        Television tv3 = new Television("Sony Tr", "SN-SONY-54321", 1399.49);
+        Television tv1 = new Television("Samsung", "QLED 75Q80A", "SN-SAM-12345", 1299.99);
+        Television tv2 = new Television("LG", "OLED55C1", "SN-LG-67890", 1499.00);
+        Television tv3 = new Television("Sony", "XR-55A80J", "SN-SONY-54321", 1399.49);
 
-
+        
         System.out.println("===== Телевизор 1 =====");
         System.out.println(tv1);
 
@@ -58,8 +72,6 @@ class App {
 
         System.out.println("\n===== Телевизор 3 =====");
         System.out.println(tv3);
-
-
 
     }
 }
